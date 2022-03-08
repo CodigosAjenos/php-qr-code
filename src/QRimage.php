fae66430b1ac7be67a135f65f20ea939237a26ca
YisusCore <43710620+YisusCore@users.noreply.github.com>
2022-03-08 16:54:31 -0500

    Update QRimage.php

--- src/QRimage.php
+++ src/QRimage.php
@@ -1,32 +1,8 @@
-﻿<?php
-/*
- * PHP QR Code encoder
- *
- * Image output of code using GD2
- *
- * PHP QR Code is distributed under LGPL 3
- * Copyright (C) 2010 Dominik Dzienia <deltalab at poczta dot fm>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*/
-
-define('QR_IMAGE', true);
+<?php
+namespace QRcode;
 
 class QRimage
 {
-
 	//----------------------------------------------------------------------
 	public static function png($frame, $filename = false, $pixelPerPoint = 4, $outerFrame = 4, $saveandprint = false)
 	{
