3d4d89ef54709da0575915622e4f60897a3a6a33
YisusCore <43710620+YisusCore@users.noreply.github.com>
2022-03-08 16:47:29 -0500

    Update QRconfig.php

--- src/QRconfig.php
+++ src/QRconfig.php
@@ -1,14 +1,16 @@
-﻿<?php
-/*
- * PHP QR Code encoder
- *
- * Config file, feel free to modify
-*/
+<?php
+namespace QRcode;
 
-define('QR_CACHEABLE', true); // use cache - more disk reads but less CPU power, masks and format templates are stored there
-define('QR_CACHE_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR); // used when QR_CACHEABLE === true
-define('QR_LOG_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR); // default error logs dir
-define('QR_FIND_BEST_MASK', true); // if true, estimates best mask (spec. default, but extremally slow; set to false to significant performance boost but (propably) worst quality code
-define('QR_FIND_FROM_RANDOM', false); // if false, checks all masks available, otherwise value tells count of masks need to be checked, mask id are got randomly
-define('QR_DEFAULT_MASK', 2); // when QR_FIND_BEST_MASK === false
-define('QR_PNG_MAXIMUM_SIZE', 1024); // maximum allowed png image width (in pixels), tune to make sure GD and PHP can handle such big images
+defined('QRcode_QR_APPPATH') or define('QRcode_QR_APPPATH', __DIR__);
+
+class QRconfig
+{
+	const QR_APPPATH          = QRcode_QR_APPPATH;
+	const QR_CACHEABLE        = true;  // use cache - more disk reads but less CPU power, masks and format templates are stored there
+	const QR_CACHE_DIR        = QRcode_QR_APPPATH . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR; // used when QR_CACHEABLE === true
+	const QR_LOG_DIR          = QRcode_QR_APPPATH . DIRECTORY_SEPARATOR; // default error logs dir
+	const QR_FIND_BEST_MASK   = true;  // if true, estimates best mask (spec. default, but extremally slow; set to false to significant performance boost but (propably) worst quality code
+	const QR_FIND_FROM_RANDOM = false; // if false, checks all masks available, otherwise value tells count of masks need to be checked, mask id are got randomly
+	const QR_DEFAULT_MASK     = 2;     // when QR_FIND_BEST_MASK === false
+	const QR_PNG_MAXIMUM_SIZE = 1024;  // maximum allowed png image width (in pixels), tune to make sure GD and PHP can handle such big images
+}
